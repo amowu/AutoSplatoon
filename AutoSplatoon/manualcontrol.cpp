02c0717f401c8fd73d80eb8a175e3a140d192d67
Marc Diaz Calderon <68778320+MDCmarc@users.noreply.github.com>
2024-04-28 23:01:42 +0200

    Update manualcontrol.cpp
    Easier and faster map lookup table, would be better to make it a class variable so it is not declared each time the function gets called.

--- AutoSplatoon/manualcontrol.cpp
+++ AutoSplatoon/manualcontrol.cpp
@@ -84,68 +84,45 @@ void ManualControl::on_buttonReleased()
 {
     emit buttonAction(_inpEmu.NO_INPUT, MANUAL);
 }
-
 void ManualControl::sendCommand(const QString& text, int interval)
 {
-    if (text == "A") {
-        emit buttonAction(_inpEmu.BTN_A, MANUAL);
-    } else if (text == "B") {
-        emit buttonAction(_inpEmu.BTN_B, MANUAL);
-    } else if (text == "X") {
-        emit buttonAction(_inpEmu.BTN_X, MANUAL);
-    } else if (text == "Y") {
-        emit buttonAction(_inpEmu.BTN_Y, MANUAL);
-    } else if (text == "Du") {
-        emit buttonAction(_inpEmu.DPAD_U, MANUAL);
-    } else if (text == "Dd") {
-        emit buttonAction(_inpEmu.DPAD_D, MANUAL);
-    } else if (text == "Dl") {
-        emit buttonAction(_inpEmu.DPAD_L, MANUAL);
-    } else if (text == "Dr") {
-        emit buttonAction(_inpEmu.DPAD_R, MANUAL);
-    } else if (text == "Home") {
-        emit buttonAction(_inpEmu.BTN_HOME, MANUAL);
-    } else if (text == "Cap") {
-        emit buttonAction(_inpEmu.BTN_CAPTURE, MANUAL);
-    } else if (text == "Sel") {
-        emit buttonAction(_inpEmu.BTN_MINUS, MANUAL);
-    } else if (text == "Str") {
-        emit buttonAction(_inpEmu.BTN_PLUS, MANUAL);
-    } else if (text == "L+R") {
-        emit buttonAction(_inpEmu.BTN_L + _inpEmu.BTN_R, MANUAL);
-    } else if (text == "CLK") {
-        emit buttonAction(_inpEmu.BTN_LCLICK, MANUAL);
-    } else if (text == "L") {
-        emit buttonAction(_inpEmu.BTN_L, MANUAL);
-    } else if (text == "R") {
-        emit buttonAction(_inpEmu.BTN_R, MANUAL);
-    } else if (text == "Lu") {
-        emit buttonAction(_inpEmu.LSTICK_U, MANUAL);
-    } else if (text == "Ld") {
-        emit buttonAction(_inpEmu.LSTICK_D, MANUAL);
-    } else if (text == "Ll") {
-        emit buttonAction(_inpEmu.LSTICK_L, MANUAL);
-    } else if (text == "Lr") {
-        emit buttonAction(_inpEmu.LSTICK_R, MANUAL);
-    } else if (text == "Ru") {
-        emit buttonAction(_inpEmu.RSTICK_U, MANUAL);
-    } else if (text == "Rd") {
-        emit buttonAction(_inpEmu.RSTICK_D, MANUAL);
-    } else if (text == "Rl") {
-        emit buttonAction(_inpEmu.RSTICK_L, MANUAL);
-    } else if (text == "Rr") {
-        emit buttonAction(_inpEmu.RSTICK_R, MANUAL);
-    } else if (text == "ADu") {
-        emit buttonAction(_inpEmu.A_DPAD_U, MANUAL);
-    } else if (text == "ADd") {
-        emit buttonAction(_inpEmu.A_DPAD_D, MANUAL);
-    } else if (text == "ADl") {
-        emit buttonAction(_inpEmu.A_DPAD_L, MANUAL);
-    } else if (text == "ADr") {
-        emit buttonAction(_inpEmu.A_DPAD_R, MANUAL);
+    const QMap<QString, int> inputMap = {
+        {"A", _inpEmu.BTN_A},
+        {"B", _inpEmu.BTN_B},
+        {"X", _inpEmu.BTN_X},
+        {"Y", _inpEmu.BTN_Y},
+        {"Du", _inpEmu.DPAD_U},
+        {"Dd", _inpEmu.DPAD_D},
+        {"Dl", _inpEmu.DPAD_L},
+        {"Dr", _inpEmu.DPAD_R},
+        {"Home", _inpEmu.BTN_HOME},
+        {"Cap", _inpEmu.BTN_CAPTURE},
+        {"Sel", _inpEmu.BTN_MINUS},
+        {"Str", _inpEmu.BTN_PLUS},
+        {"L+R", _inpEmu.BTN_L + _inpEmu.BTN_R},
+        {"CLK", _inpEmu.BTN_LCLICK},
+        {"L", _inpEmu.BTN_L},
+        {"R", _inpEmu.BTN_R},
+        {"Lu", _inpEmu.LSTICK_U},
+        {"Ld", _inpEmu.LSTICK_D},
+        {"Ll", _inpEmu.LSTICK_L},
+        {"Lr", _inpEmu.LSTICK_R},
+        {"Ru", _inpEmu.RSTICK_U},
+        {"Rd", _inpEmu.RSTICK_D},
+        {"Rl", _inpEmu.RSTICK_L},
+        {"Rr", _inpEmu.RSTICK_R},
+        {"ADu", _inpEmu.A_DPAD_U},
+        {"ADd", _inpEmu.A_DPAD_D},
+        {"ADl", _inpEmu.A_DPAD_L},
+        {"ADr", _inpEmu.A_DPAD_R}
+    };
+
+    if (inputMap.contains(text)) {
+        emit buttonAction(inputMap.value(text), MANUAL);
     } else {
         emit buttonAction(_inpEmu.NO_INPUT, MANUAL);
     }
+
     Delay_MSec(interval);
     emit buttonAction(_inpEmu.NO_INPUT, MANUAL);
     Delay_MSec(interval);
